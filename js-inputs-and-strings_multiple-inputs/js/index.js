99384a946d747b24f3edfa0837612ee88f3bd50e
OguzKabasakal <oguzkabasakal@gmail.com>
2023-11-14 16:48:17 +0100

    solution update for js input strings - multiple inputs

--- js-inputs-and-strings_multiple-inputs/js/index.js
+++ js-inputs-and-strings_multiple-inputs/js/index.js
@@ -15,21 +15,15 @@ in uppercase into the second input field
 
 const firstInput = document.querySelector("[data-js=first-input]");
 const secondInput = document.querySelector("[data-js=second-input]");
-const button1 = document.querySelector("[data-js=button1]");
-const button2 = document.querySelector("[data-js=button2]");
-const button3 = document.querySelector("[data-js=button3]");
+const button = document.querySelector("[data-js=button1]");
 
-button1.addEventListener("click", () => {
+button.addEventListener("click", () => {
   secondInput.value = secondInput.value + firstInput.value;
-});
+  secondInput.value = secondInput.value.toUpperCase();
 
-button2.addEventListener("click", () => {
-  secondInput.value = secondInput.value + firstInput.value.toUpperCase();
-});
-
-button3.addEventListener("click", () => {
   let x = firstInput.value;
   let y = secondInput.value;
+
   firstInput.value = y;
   secondInput.value = x;
 });
