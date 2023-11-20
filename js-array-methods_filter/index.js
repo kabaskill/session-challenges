0dcd4f6e72541f198312de1145a75f9ed2cdbd57
OguzKabasakal <oguzkabasakal@gmail.com>
2023-11-20 12:25:32 +0100

    js-array-methods solutions

---
+++ js-array-methods_filter/index.js
@@ -0,0 +1,43 @@
+console.clear();
+
+const cards = [
+  {
+    id: "1",
+    question: "How often can I use <header>?",
+    answer: "As often as you like.",
+    tags: ["html", "elements", "basic"],
+  },
+  {
+    id: "2",
+    isBookmarked: false,
+    question: "How often can I use <aside>?",
+    answer: "As often as you like.",
+    tags: ["html", "elements", "basic"],
+  },
+  {
+    id: "3",
+    isBookmarked: true,
+    question: "On which types can I use destructuring assignment?",
+    answer: "On Objects and Arrays",
+    tags: ["js", "next"],
+  },
+];
+
+const onlyCardWithIdTwo = cards.filter((card) => card.id === "2");
+
+const allCardsWith3Tags = cards.filter((card) => card.tags.length === 3);
+
+const allCardsThatAreNotBookmarked = cards.filter((card) => !card.isBookmarked);
+
+const allCardsWithTagsHTMLOrJSThatAreBookmarked = cards.filter(
+  (card) =>
+    (card.tags.includes("html") || card.tags.includes("js")) &&
+    card.isBookmarked
+);
+
+export {
+  onlyCardWithIdTwo,
+  allCardsWith3Tags,
+  allCardsThatAreNotBookmarked,
+  allCardsWithTagsHTMLOrJSThatAreBookmarked,
+};
